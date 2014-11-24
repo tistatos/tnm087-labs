0b502012d2a63880ea26d8f079ee32e22ec234df
tistatos <tistatos@gmail.com>
2014-11-24 11:49:50 +0100

    more comments

--- lab1.m
+++ lab1.m
@@ -1,19 +1,23 @@
 %%
 % Lab 1 - HDR
 %%
+
+% Clear any saved data
 clear
 
+% exposure ratio between images
 exposureRatio = 2;
-
+% load g function = log2(f^-1(z_ij))
 load('gfun.mat');
 
-%%% plot g function
+% plot g function
 subplot(2,1,1);
 plot(gfun);
 subplot(2,1,2);
 plot(2.^gfun);
-%%
-% read in pictures
+
+
+% read picture into an array
 for i=1:14
     pictures(:,:,:,i) = imread(strcat('Img',num2str(i),'.tiff'));
     gPictures(:,:,i) = rgb2gray(pictures(:,:,:,i));
@@ -22,8 +26,10 @@ end
 % get max/median/min values
 hiIntVal = max(max(gPictures(:,:,1)));
 [hiIntY, hiIntX] = find (gPictures(:,:,1) == hiIntVal);
+
 loIntVal = min(min(gPictures(:,:,14)));
 [loIntY, loIntX] = find (gPictures(:,:,14) == loIntVal);
+
 medianGray = median(median(gPictures(:,:,9)));
 [medianGrayY, medianGrayX] = find(gPictures(:,:,9) == medianGray);
 
@@ -34,22 +40,26 @@ for i=1:14
     medianValues(i) = gPictures(medianGrayY(1,1), medianGrayX(1,1), i);
 end
 
-%%% Plot hi,low and median
-% figure
-% plot(hiIntValues, 'r')
-% hold on
-% plot(medianValues, 'g')
-% plot(loIntValues, 'b')
+% Plot hi,low and median
+figure
+plot(hiIntValues, 'r')
+hold on
+plot(medianValues, 'g')
+plot(loIntValues, 'b')
+%%
+figure
 tic
 
-
+%calculate weight image (could be done with one pic-loop but it doesnt seem to work)
 weight = pictures;
 for pic=1:14
     for y=1:683
         for x=1:1024
             for c=1:3
+
                 value = pictures(y,x,c,pic);
-                if(value > (255/2))
+                %weight if value is greater than 128
+                if(value > 128)
                     value = 255-value;
                 end
                 weight(y,x,c,pic) = value;
@@ -58,25 +68,30 @@ for pic=1:14
     end
 end
 
+%summarize all pictures in to one weight picture
 weightPic = weight(:,:,:,1);
 for pic=2:14
    weightPic = weightPic + weight(:,:,:,pic);
 end
 
-weightPic= double(weightPic)/255;
+%normalize weight values
+weightPic= double(weightPic)/(255);
 
+%Get irradiance values from gfun and weight it
 irradiancePictures = double(pictures);
 finv= (2.^gfun);
 for pic=1:14
     value = pictures(:,:,:,pic);
-    irValue = finv(value+1)/(2*(pic));
+    irValue = finv(value+1)/(exposureRatio*pic);
     irradiancePictures(:,:,:,pic) = irValue.*weightPic(:,:,:);
 end
 
+%Summarize to finaly picture
 finalpic = irradiancePictures(:,:,:,1);
-
 for pic=2:14
    finalpic = imadd(finalpic,irradiancePictures(:,:,:,pic));
 end
+
+%tonemap and show the result
 imshow(tonemap(finalpic));
 toc
