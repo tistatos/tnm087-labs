d771acc29d37693128d3adc350fb30146c9f8a71
Carl Englund <englund92@gmail.com>
2014-12-11 19:33:44 +0100

    Cleaned up code and removed lines.

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,134 +1,58 @@
-% Lab 3 - Operations in the fourier domain
-clear 
+clear
 
-pictures(:,:,1) = double(rgb2gray(imread('HalfCanon.jpg')));
-pictures(:,:,2) = double(rgb2gray(imread('HalfHolga.jpg')));
-pictures(:,:,3) = double(rgb2gray(imread('HalfScanner.jpg')));
-pictures(:,:,4) = double(rgb2gray(imread('HalfSony.jpg')));
+%%Read in images, convert to double, grayscale and values between 0 to 1
+pictures(:,:,1) = mat2gray(double(rgb2gray(imread('HalfCanon.jpg'))));
+pictures(:,:,2) = mat2gray(double(rgb2gray(imread('HalfHolga.jpg'))));
+pictures(:,:,3) = mat2gray(double(rgb2gray(imread('HalfScanner.jpg'))));
+pictures(:,:,4) = mat2gray(double(rgb2gray(imread('HalfSony.jpg'))));
 
+%%Construct a weight vector that looks like a V
 SharpWeight = zeros (1, 512);
 for i=1:255
     SharpWeight(256-i) = i/255;
     SharpWeight(257+i) = i/255;
 end
 
-%{
-plot(SharpWeight)
-
-for i = 1:4
-    pictures(:,:,i) = pictures(:,:,i)/max(max(pictures(:,:,i)));
-    EdgePicture = pictures(1:50, :, i);
-    ShiftedImages = fftshift(fft(padarray(sum(EdgePicture), [0 128])));
-    ShiftedImages = ShiftedImages/ShiftedImages(256);
-    Sharpness(i) = sum(abs(ShiftedImages).*SharpWeight);
-end
-%}
-HalfCanon = pictures(:,:,1)/max(max(pictures(:,:,1)));
-HalfHolga = pictures(:,:,2)/max(max(pictures(:,:,2)));
-HalfScanner = pictures(:,:,3)/max(max(pictures(:,:,3)));
-HalfSony = pictures(:,:,4)/max(max(pictures(:,:,4)));
-
-HalfCanon = mat2gray(HalfCanon);
-HalfHolga = mat2gray(HalfHolga);
-HalfScanner = mat2gray(HalfScanner);
-HalfSony = mat2gray(HalfSony);
-
-EdgeCanon = HalfCanon(1:50, :);
-EdgeHolga = HalfHolga(1:50, :);
-EdgeScanner = HalfScanner(1:50, :);
-EdgeSony = HalfSony(1:50, :);
-
-padCanon = padarray(sum(EdgeCanon), [0 128]);
-padHolga = padarray(sum(EdgeHolga), [0 128]);
-padScanner = padarray(sum(EdgeScanner), [0 128]);
-padSony = padarray(sum(EdgeSony), [0 128]);
-
-FFT1EdgeCanon = fftshift(fft(padCanon));
-FFT1EdgeHolga = fftshift(fft(padHolga));
-FFT1EdgeScanner = fftshift(fft(padScanner));
-FFT1EdgeSony = fftshift(fft(padSony));
-
-%plot(real(FFT1EdgeCanon), imag(FFT1EdgeCanon));
-%plot(real(FFT1EdgeHolga), imag(FFT1EdgeHolga));
-%plot(real(FFT1EdgeScanner), imag(FFT1EdgeScanner));
-%plot(real(FFT1EdgeSony), imag(FFT1EdgeSony));
-
-AbsFFT1EdgeCanon = abs(FFT1EdgeCanon./FFT1EdgeCanon(256));
-AbsFFT1EdgeHolga = abs(FFT1EdgeHolga./FFT1EdgeHolga(256));
-AbsFFT1EdgeScanner = abs(FFT1EdgeScanner./FFT1EdgeScanner(256));
-AbsFFT1EdgeSony = abs(FFT1EdgeSony./FFT1EdgeSony(256));
-
-CanonSharp = sum(AbsFFT1EdgeCanon.*SharpWeight);
-HolgaSharp = sum(AbsFFT1EdgeHolga.*SharpWeight);
-ScannerSharp = sum(AbsFFT1EdgeScanner.*SharpWeight);
-SonySharp = sum(AbsFFT1EdgeSony.*SharpWeight);
-
-plot(SharpWeight)
-T1 = table(CanonSharp, HolgaSharp, ScannerSharp, SonySharp);
-
-%Sharpness
-
-%% Part b
-EdgeCanon2 = padarray(HalfCanon, [128 128]);
-EdgeHolga2 = padarray(HalfHolga, [128 128]);
-EdgeScanner2 = padarray(HalfScanner, [128 128]);
-EdgeSony2 = padarray(HalfSony, [128 128]);
-
-EdgeCanon2 = fftshift(fft2(EdgeCanon2));
-EdgeHolga2 = fftshift(fft2(EdgeHolga2));
-EdgeScanner2 = fftshift(fft2(EdgeScanner2));
-EdgeSony2 = fftshift(fft2(EdgeSony2));
-
-AbsEdgeCanon2 = abs(EdgeCanon2./EdgeCanon2(256));
-AbsEdgeHolga2 = abs(EdgeHolga2./EdgeHolga2(256));
-AbsEdgeScanner2 = abs(EdgeScanner2./EdgeScanner2(256));
-AbsEdgeSony2 = abs(EdgeSony2./EdgeSony(256));
-
-%plot(AbsEdgeCanon2, 'red');
-%hold on
-%plot(AbsEdgeHolga2, 'blue');
-%hold on
-%plot(AbsEdgeScanner2, 'green');
-%hold on
-%plot(AbsEdgeSony2, 'black');
-
-SharpWeight = zeros (1, 100);
-for i=1:50
-    SharpWeight(51-i) = i/50;
-    SharpWeight(50+i) = i/50;
-end
-
-%Calculate sharpness
+%%Meshgrid and convert to polar coordinates.
 N = 512;
 [X,Y] = meshgrid((1:N));
 [T,R] = cart2pol(X-N/2,Y-N/2);
 
-SR = R ./ R(N/2 - 1, 1);
-
 %Quantize to array with values between 1-100
-QR = uint8(round(100*(SR./max(SR(:)))));
-
-
-for m = 1:100  
-    Maskm = (QR == m);
-    masksum = sum(sum(Maskm));
+QR = uint8(round(100*(R./max(R(:)))));
+for i = 1:4
+    %%Padarray, fft/fft2, and shift the images, also have values between 0 and 1
+    ShiftedImage = fftshift(fft(padarray(sum(pictures(1:50, :, i)), [0 128])));
+    ShiftedImage2 = fftshift(fft2(padarray(pictures(:,:,i), [128 128])));
     
-    CanonMask = AbsEdgeCanon2(Maskm == 1);
-    HolgaMask = AbsEdgeHolga2(Maskm == 1);
-    ScannerMask = AbsEdgeScanner2(Maskm == 1);
-    SonyMask = AbsEdgeSony2(Maskm == 1);
-
-    CanonMaskAvg(m) = mean(CanonMask); 
-    HolgaMaskAvg(m) = mean(HolgaMask);
-    ScannerMaskAvg(m) = mean(ScannerMask);
-    SonyMaskAvg(m) = mean(SonyMask);
+    %Normalize images with the DC-component
+    %PlotImage = ShiftedImage;
+    %PlotImage2 = ShiftedImage2;
+    ShiftedImage = abs(ShiftedImage./ShiftedImage(256));
+    ShiftedImage2 = abs(ShiftedImage2./ShiftedImage2(256));
+    
+    %%Masking for second task in part one.
+    for m = 1:100
+        Maskm = (QR == m);
+        masksum = sum(sum(Maskm));
+        MaskedImage = ShiftedImage2(Maskm == 1);
+        MaskedImageAvg(m) = mean(MaskedImage);
+    end
+    
+    %Weight images and calculate sharpness
+    Sharpness1(i) = sum(ShiftedImage.*SharpWeight);
+    Sharpness2(i) = sum(MaskedImageAvg/max(MaskedImageAvg(:)).*(1:100));
 end
 
-
-CanonSharp = sum(CanonMaskAvg/max(CanonMaskAvg(:)).*SharpWeight)
-HolgaSharp = sum(HolgaMaskAvg/max(HolgaMaskAvg(:)).*SharpWeight)
-ScannerSharp = sum(ScannerMaskAvg/max(ScannerMaskAvg(:)).*SharpWeight)
-SonySharp = sum(SonyMaskAvg/max(SonyMaskAvg(:)).*SharpWeight)
-
-
-T2 = table(CanonSharp, HolgaSharp, ScannerSharp, SonySharp);
+%Plots for the labassignment for the Sony.
+%{
+subplot(3,1,1);
+plot(real(PlotImage));
+legend('Sony real part');
+subplot(3,1,2);
+plot(imag(PlotImage));
+legend('Sony imaginary part');
+subplot(3,1,3);
+plot(abs(PlotImage));
+legend('Sony absolute');
+%}
--- lab3/lab3part2.m
+++ lab3/lab3part2.m
@@ -1,19 +1,18 @@
 clear all
 load('winsuint8.mat')
 
+%Create a meshgrid that is 64x64
 N = 64;
 [X,Y] = meshgrid((1:N));
 [T,R] = cart2pol(X-N/2,Y-N/2);
 
+%Mask images with the grid, perform same operations as in part 1 on
+%pictures, then take sum(sum(radial values .* pictures))
 for m = 1:192
-    
     pictures = fftshift(fft2(padarray(winsuint8(:,:,m), [16 16])));
     AbsPictures = abs(pictures/abs(pictures(33,33)));
- 
-    SR = R ./ R(N/2 - 1, 1);
-    Sharpness(m) = sum(sum(SR.*AbsPictures));
-    
+    Sharpness(m) = sum(sum(R.*AbsPictures));
 end
 
-
+%Plot values
 plot(Sharpness)
